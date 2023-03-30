56d619c4a5eb7ffcc8b6fab73301994cf2f1fb85
Chronet <jaeducaba@gmail.com>
2023-03-30 11:35:22 -0500

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'ecosystem-dev-f4893',
+    appId: '1:965227033049:web:51ba48888ba0772607a9c8',
+    storageBucket: 'ecosystem-dev-f4893.appspot.com',
+    apiKey: 'AIzaSyA55v7qByZHMbsVcnTN_AgaHuCDWqig1_Q',
+    authDomain: 'ecosystem-dev-f4893.firebaseapp.com',
+    messagingSenderId: '965227033049',
+  },
+};
