e347c3b52ac5d2f89210a531ece80d3a082dfffc
Jairo <jairo.carmona@datapeers.co>
2023-04-11 10:33:57 -0500

    adjust env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: true,
-  api: 'https://vinku.net/back',
-  graphQL: 'https://vinku.net/back/graphql',
-  graphqlSubscription: 'wss://vinku.net/backsub/subscriptions',
+  production: false,
+  api: 'http://localhost:3000',
+  graphql: 'http://localhost:3000/graphql',
+  graphqlSubscription: 'ws://localhost:2500/subscriptions',
   firebase: {
     projectId: 'ecosystem-dev-f4893',
     appId: '1:965227033049:web:51ba48888ba0772607a9c8',
