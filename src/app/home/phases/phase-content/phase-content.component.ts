7775d0225e08a1789582f18f89be7ace235daece
Jairo <jairo.carmona@datapeers.co>
2023-04-26 09:51:41 -0500

    a

--- src/app/home/phases/phase-content/phase-content.component.ts
+++ src/app/home/phases/phase-content/phase-content.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AppState } from '@appStore/app.reducer';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-phase-content',
   templateUrl: './phase-content.component.html',
-  styleUrls: ['./phase-content.component.scss']
+  styleUrls: ['./phase-content.component.scss'],
 })
-export class PhaseContentComponent {
+export class PhaseContentComponent implements OnInit, OnDestroy {
+  constructor(private store: Store<AppState>) {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+  }
 }
