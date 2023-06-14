118f67bd0617419eb054ed684b50483391f80629
Jairo <jairo.carmona@datapeers.co>
2023-06-14 14:14:26 -0500

    working in events

--- src/app/home/phases/model/events.model.ts
+++ src/app/home/phases/model/events.model.ts
@@ -57,11 +57,68 @@ export class TypeEvent implements ITypeEvent {
 export interface IEvent {
   _id: string;
   name: string;
-  extra_options: any;
-  isDeleted: boolean;
   type: string;
+  extra_options: any;
   startAt: Date;
   endAt: Date;
+  phase: string;
   createdAt: Date;
   updatedAt: Date;
+  isDeleted: boolean;
+}
+
+export class Event implements IEvent {
+  _id: string;
+  name: string;
+  type: string;
+  extra_options: any;
+  startAt: Date;
+  endAt: Date;
+  phase: string;
+  createdAt: Date;
+  updatedAt: Date;
+  isDeleted: boolean;
+
+  private constructor() {}
+
+  static fromJson(data: IEvent): Event {
+    const content = new Event();
+    Object.assign(content, {
+      ...data,
+      createdAt: new Date(data.createdAt),
+      updatedAt: new Date(data.updatedAt),
+      startAt: new Date(data.createdAt),
+      endAt: new Date(data.createdAt),
+      extra_options: {
+        ...data?.extra_options,
+      },
+    });
+    return content;
+  }
+
+  toSave(): Partial<Event> {
+    return {
+      _id: this._id,
+      name: this.name,
+      extra_options: this.extra_options,
+      startAt: this.startAt,
+      endAt: this.endAt,
+      isDeleted: this.isDeleted,
+    };
+  }
+
+  static newEvent() {
+    return {
+      name: '',
+      type: '',
+      startAt: new Date(),
+      endAt: new Date(),
+      phase: '',
+      extra_options: {
+        assistant: 'onsite',
+        description: '',
+        url: '',
+      },
+    };
+  }
 }
