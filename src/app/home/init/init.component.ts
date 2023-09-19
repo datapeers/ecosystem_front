46474729003806610d177d1ef7b8c141f1ceda71
Jairo <jairo.carmona@datapeers.co>
2023-09-19 09:37:06 -0500

    added shape of states to map init

--- src/app/home/init/init.component.ts
+++ src/app/home/init/init.component.ts
@@ -11,7 +11,8 @@ import { ConfigurationService } from '@home/configuration/configuration.service'
 import { ConfigurationApp } from '@home/configuration/model/configurationApp';
 import { Store } from '@ngrx/store';
 import { first, firstValueFrom } from 'rxjs';
-import { Map, tileLayer, Marker, Icon } from 'leaflet';
+import { Map, tileLayer, Marker, Icon, geoJSON } from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-init',
@@ -23,11 +24,13 @@ export class InitComponent implements OnInit, OnDestroy, AfterViewInit {
   config: ConfigurationApp;
   mainMap: Map;
   latLong = [5.0710225, -75.4927164];
+  private states;
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.resizeMap();
   }
   constructor(
+    private http: HttpClient,
     private store: Store<AppState>,
     private serviceConfig: ConfigurationService
   ) {
@@ -73,14 +76,34 @@ export class InitComponent implements OnInit, OnDestroy, AfterViewInit {
     }
   }
 
-  initializeMainMap() {
+  async initializeMainMap() {
     if (!this.mainMap) {
-      this.mainMap = new Map('mapSite').setView([0, 0], 1.5);
+      this.states = await this.getStateShapes();
+      const stateLayer = geoJSON(this.states, {
+        style: (feature) => ({
+          weight: 3,
+          opacity: 0.5,
+          color: '#008f68',
+          fillOpacity: 0.8,
+          fillColor: '#6DB65B',
+        }),
+      });
+      this.mainMap = new Map('mapSite', { zoomControl: false }).setView(
+        [0, 0],
+        1.5
+      );
       var cartodbAttribution =
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
       tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         attribution: cartodbAttribution,
       }).addTo(this.mainMap);
+      this.mainMap.addLayer(stateLayer);
     }
+  }
+
+  getStateShapes() {
+    return firstValueFrom(
+      this.http.get('../../../assets/data/gz_2010_us_outline_5m.json')
+    );
   }
 }
